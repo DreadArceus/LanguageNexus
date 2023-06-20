8e6984af81a0ec83efa3ad3f66348f2465ddf22d
dreadarceus <dreadarceus@gmail.com>
2023-06-20 06:40:23 +0530

    Setting up Integration Testing 1.3

--- nexus/src/constants.ts
+++ nexus/src/constants.ts
@@ -1,13 +1,11 @@
-import { languages } from "./types";
+import type { languages } from "./types";
 
-export const __prod__ = process.env.NODE_ENV === "production";
-export const nexusPort = __prod__ ? Number(process.env.NEXUS_PORT) : 4000;
-export const typescriptPort = __prod__
-  ? Number(process.env.TYPESCRIPT_PORT)
-  : 4001;
-export const pythonPort = __prod__ ? Number(process.env.PYTHON_PORT) : 4002;
-export const rubyPort = __prod__ ? Number(process.env.RUBY_PORT) : 4003;
-export const goPort = __prod__ ? Number(process.env.GO_PORT) : 4004;
+export const PROD = process.env.NODE_ENV === "production";
+export const nexusPort = PROD ? Number(process.env.NEXUS_PORT) : 4000;
+export const typescriptPort = PROD ? Number(process.env.TYPESCRIPT_PORT) : 4001;
+export const pythonPort = PROD ? Number(process.env.PYTHON_PORT) : 4002;
+export const rubyPort = PROD ? Number(process.env.RUBY_PORT) : 4003;
+export const goPort = PROD ? Number(process.env.GO_PORT) : 4004;
 
 export const langInfo: Record<languages, number> = {
   TypeScript: typescriptPort,
--- nexus/src/index.ts
+++ nexus/src/index.ts
@@ -1,27 +1,39 @@
 import express from "express";
 import { langInfo, nexusPort } from "./constants";
-import { languages } from "./types";
+import type { languages } from "./types";
+import type { Request, Response, NextFunction, RequestHandler } from "express";
 
-const main = async () => {
+const asyncHandler =
+  (
+    fn: (req: Request, res: Response, next: NextFunction) => Promise<void>
+  ): RequestHandler =>
+  (req, res, next) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
+
+const main = async (): Promise<void> => {
   const app = express();
 
-  app.get("/ping", async (_, res) => {
-    const results: Partial<Record<languages, Object>> = {};
+  app.get(
+    "/ping",
+    asyncHandler(async (_, res) => {
+      const results: Partial<Record<languages, unknown>> = {};
 
-    const promises = Object.entries(langInfo).map(async ([lang, port]) => {
-      try {
-        const res = await fetch(`http://localhost:${port}/ping`);
-        const data = await res.json();
-        results[lang as languages] = data;
-      } catch (err) {
-        console.error(err);
-        results[lang as languages] = { error: "Unable to reach API" };
-      }
-    });
+      const promises = Object.entries(langInfo).map(async ([lang, port]) => {
+        try {
+          const res = await fetch(`http://localhost:${port}/ping`);
+          const data = await res.json();
+          results[lang as languages] = data;
+        } catch (err) {
+          console.error(err);
+          results[lang as languages] = { error: "Unable to reach API" };
+        }
+      });
 
-    await Promise.all(promises);
-    res.send(results);
-  });
+      await Promise.all(promises);
+      res.send(results);
+    })
+  );
 
   app.listen(nexusPort, () => {
     console.log(`Nexus is listening on ${nexusPort}`);
