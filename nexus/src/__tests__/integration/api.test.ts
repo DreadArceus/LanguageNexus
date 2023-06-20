15f3914bee94801dc6c71429eaf369bf5ded4679
dreadarceus <dreadarceus@gmail.com>
2023-06-21 02:30:20 +0530

    Tracking Time

--- nexus/src/__tests__/integration/api.test.ts
+++ nexus/src/__tests__/integration/api.test.ts
@@ -3,12 +3,18 @@ describe("Nexus API Endpoints", () => {
     const response = await fetch("http://localhost:4000/ping");
     expect(response.status).toBe(200);
 
-    const json = await response.json();
-    Object.values(json).forEach((data) => {
-      expect(data).toHaveProperty("result");
+    const { results, stats } = await response.json();
 
+    Object.values(results).forEach((data) => {
+      expect(data).toHaveProperty("result");
       const typedData = data as Record<string, unknown>;
       if ("result" in typedData) expect(typedData.result).toEqual("pong");
+    });
+
+    Object.values(stats).forEach((data) => {
+      expect(data).toBeDefined();
+      expect(typeof data).toBe("number");
+      expect(data).toBeGreaterThanOrEqual(0);
     });
   });
 });
--- nexus/src/index.ts
+++ nexus/src/index.ts
@@ -17,20 +17,26 @@ app.get(
   "/ping",
   asyncHandler(async (_, res) => {
     const results: Partial<Record<languages, unknown>> = {};
+    const stats: Partial<Record<languages, number | "error">> = {};
 
-    const promises = Object.entries(langInfo).map(async ([lang, port]) => {
+    const promises = Object.entries(langInfo).map(async ([rawLang, port]) => {
+      const lang = rawLang as languages;
       try {
+        const startTime = Date.now();
         const res = await fetch(`http://127.0.0.1:${port}/ping`);
+        const endTime = Date.now();
         const data = await res.json();
-        results[lang as languages] = data;
+        results[lang] = data;
+        stats[lang] = endTime - startTime;
       } catch (err) {
         console.error(err);
-        results[lang as languages] = { error: `Unable to reach ${lang} API` };
+        results[lang] = { error: `Unable to reach ${lang} API` };
+        stats[lang] = "error";
       }
     });
 
     await Promise.all(promises);
-    res.send(results);
+    res.send({ results, stats });
   })
 );
 
